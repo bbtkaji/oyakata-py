6a7c8fb0f7a66c662985e402bf0d508dca23f65f
oyakata <echizen@ubuntu.(none)>
2013-01-16 12:32:11 +0900

    A skeleton.

--- vol3/src/app/tanka100/api.py
+++ vol3/src/app/tanka100/api.py
@@ -1,19 +1,13 @@
 # -*- coding:utf-8 -*-
-from urllib import urlencode
-from urllib2 import urlopen
-import json
-from contextlib import closing
-
-
-URL = r'http://api.aoikujira.com/hyakunin/get.php?{query}'
 
 
 def findpoem(keyword):
-    """百人一首APIを問い合わせて結果を辞書のリストで返す。"""
-    values = {"fmt": "json",
-              "key": keyword.encode("utf-8")
-                     if isinstance(keyword, unicode) else keyword}
-    url = URL.format(query=urlencode(values))
-    with closing(urlopen(url)) as response:
-        rows = json.loads(response.read())
-    return rows
+    """百人一首APIを問い合わせて結果を辞書のリストで返す。
+    
+    クジラ 百人一首 API
+    http://api.aoikujira.com/hyakunin/
+
+    + パラメータ"fmt"で出力フォーマットを指定できる。
+    + パラメータ"key"で部分一致検索できる。
+    """
+    return []  # stub
--- vol3/src/app/top/views.py
+++ vol3/src/app/top/views.py
@@ -1,10 +1,4 @@
 # -*- coding:utf-8 -*-
-import random
-
-from django.views.generic import View
-from django.template.response import TemplateResponse
-
-import app.tanka100.api as tanka_api
 
 
 HIRAGANA = (u"あいうえお"
@@ -19,13 +13,4 @@ HIRAGANA = (u"あいうえお"
             )
 
 
-class Index(View):
-    http_method_names = ["get"]
-    template = "top/index.html"
-
-    def get(self, request, *args, **kwargs):
-        """ランダムにひらがな一文字を指定して百人一首を検索する。"""
-        keyword = random.choice(HIRAGANA)
-        rows = tanka_api.findpoem(keyword)
-        context = {"rows": rows, "letter": keyword}
-        return TemplateResponse(request, self.template, context)
+# Index ランダムにひらがな一文字を指定して百人一首を検索する。
